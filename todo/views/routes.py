1e859430ec65054ddb8cee4721ad82676a97d8dc
zxcv2008a <zxcv2008a@gmail.com>
2023-02-22 12:23:11 +0900

    First week practical

---
+++ todo/views/routes.py
@@ -0,0 +1,61 @@
+from flask import Blueprint
+from flask import jsonify
+
+api = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
+@ api.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+
+@ api.route('/todo', methods=['POST'])
+def create_todo():
+    return jsonify({
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": False,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })
+
+
+@ api.route('/todo/<int:id>', methods=['GET'])
+def get_todo(id):
+    return jsonify({
+        "id": id,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": False,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })
+
+
+@ api.route('/todo/1', methods=['PUT'])
+def update_todo():
+    return jsonify({
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": False,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })
+
+
+@ api.route('/todo/1', methods=['DELETE'])
+def delete_todo():
+    return jsonify({
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": False,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })
